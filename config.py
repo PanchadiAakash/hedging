f58308d725ebf9a4b92a79f82eb843f5d1122260
PanchadiAakash <144662448+PanchadiAakash@users.noreply.github.com>
2025-10-28 18:58:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ METABASE_USERNAME = 'dataupload@zetwerk.com'
 METABASE_PASSWORD = "A7GeW4Jzk9BpwD2"
 
 POSTGRES_DB =  {
-    'dbname': 'postgres',
+    'database': 'postgres',
     'user': 'data_team_aakash',
     'password': 'EwgcFoml8QZsQMmxJT',
     'host': 'data-hub-postgres-uat.ci15lz2txwxa.ap-south-1.rds.amazonaws.com',
